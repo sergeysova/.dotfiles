b0492340d09f7f99e3d3d71128606b5d9bbc9e3e
lestad <i.am@lestad.net>
2016-09-10 22:49:02 +0300

    Add zsh config

--- install.sh
+++ install.sh
@@ -11,12 +11,14 @@ mkdir ~/.dotfiles_old
 mv ~/.bashrc	  ~/.dotfiles_old/bashrc
 mv ~/.npmrc	  ~/.dotfiles_old/npmrc
 mv ~/.gitconfig ~/.dotfiles_old/gitconfig
+mv ~/.zshrc ~/.dotfiles_old/zshrc
 [ "$(ls -A ~/.dotfiles_old)" ] && echo -e "\033[0;32mbackup to ~/.dotfiles_old\033[0m" || rm -r ~/.dotfiles_old
 
 # install good dotfiles
 ln -s ~/.dotfiles/bashrc      ~/.bashrc
 ln -s ~/.dotfiles/npmrc       ~/.npmrc
 ln -s ~/.dotfiles/gitconfig   ~/.gitconfig
+ln -s ~/.dotfiles/zshrc       ~/.zshrc
 echo -e "\033[0;32mgood dotfiles well installed!\033[0m"
 
 source ~/.bashrc
